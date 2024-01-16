35cc39dbbb74d9e3e53c1ac8511f96eec46fccda
Wahyudi Umar <wahyucherryorlove@gmail.com>
2024-01-16 14:00:38 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h2 class="text-red-400">testing</h2>
+</section>
+
+<style></style>
